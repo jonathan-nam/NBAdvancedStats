0787c653eefdd939bfdafb9f76d80efe56233159
Jonathan <jonathan.nam14@gmail.com>
2018-11-17 19:37:43 -0500

    small changes

---
+++ StatsNBA/src/Player.java
@@ -0,0 +1,21 @@
+
+public class Player {
+	private String playerLink;
+	private String name;
+	
+	public Player (String name) {
+		this.name = name;
+	}
+	
+	public String getPlayerLink() {
+		return playerLink;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String toString() {
+		return name;
+	}
+}
--- StatsNBA/src/test.java
+++ StatsNBA/src/test.java
@@ -6,7 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class test {
-	private static final int TOP_PLAYERS = 50;
+	private static final int NUM_PLAYERS = 50;
+	private static final int NUM_TABLES = 4;
 	private static ArrayList <WebElement> players = new ArrayList<>();
 	
 	public static void main (String [] args) {
@@ -19,8 +20,9 @@ public class test {
 		WebDriver browser = new ChromeDriver();
 		browser.get("https://stats.nba.com/leaders/");
 		
-		for (int i = 1; i <= TOP_PLAYERS; i++) {
-			WebElement player = browser.findElement(By.xpath(
+		for (int i = 1; i <= NUM_PLAYERS; i++) {
+			WebElement player = 
+					browser.findElement(By.xpath(
 							"/html/body/main/div[2]/div/div[2]/div/div/nba-stat-table/div[2]/div[1]/table/tbody/tr["
 							+ i
 							+ "]/td[2]/a"));
@@ -31,8 +33,20 @@ public class test {
 			System.out.println(p.getText());
 		}
 	}
+	
+	public Object[][] getTopPPG() {
+		Object[][] topPlayers = new Object[10][10];
+		
+		
+		
+		return topPlayers;
+	}
 }
 
 //XPATH
+///html/body/main/div[2]/div/div[2]/div/div/nba-stat-table/div[2]/div[1]/table/tbody/tr[46]/td[2]/a
 //1 - /html/body/main/div[2]/div/div[2]/div/div/nba-stat-table/div[2]/div[1]/table/tbody/tr[1]/td[2]/a
 //2 - /html/body/main/div[2]/div/div[2]/div/div/nba-stat-table/div[2]/div[1]/table/tbody/tr[2]/td[2]/a
+//50 - /html/body/main/div[2]/div/div[2]/div/div/nba-stat-table/div[2]/div[1]/table/tbody/tr[50]/td[2]/a
+// 51 - /html/body/main/div[2]/div/div[2]/div/div/nba-stat-table/div[2]/div[1]/table/tbody/tr[51]/td[2]/a
+//#per_game_stats > tbody > tr:nth-child(1) > td:nth-child(2)
